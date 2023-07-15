9afb85f7fcadb03a772d4c6fed35823e2597d9cc
Alexander Faus <alexfaus08@gmail.com>
2023-07-15 12:45:36 -0400

    feat(Section): add id prop

--- src/components/Section.svelte
+++ src/components/Section.svelte
@@ -1,8 +1,9 @@
 <script>
+	export let id = '';
 	export let bgColor = 'bg-initial';
 </script>
 
-<section class={bgColor}>
+<section class={bgColor} {id}>
 	<div class="max-w-7xl mx-auto p-8 lg:p-14">
 		<slot />
 	</div>
