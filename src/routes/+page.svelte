d379c0189db535d6f60b720840d7ada7b2542b1f
Alexander Faus <alexfaus08@gmail.com>
2023-07-15 10:22:04 -0400

    feat(homepage): use TextWithImage component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,11 @@
 <script>
 	export let data;
 
-	import imageUrlBuilder from '@sanity/image-url';
-	import { default as client } from '$lib/sanity.ts';
-
-	console.log(data);
-	const builder = imageUrlBuilder(client);
-
-	function imageFor(imgSrc) {
-		console.log(imgSrc);
-		return builder.image(imgSrc);
-	}
+	import TextWithImage from '../components/TextWithImage.svelte';
 </script>
 
-<div class="p-20">
-	<div class="flex flex-row items-center">
-		<div
-			class="flex flex-col items-center gap-4 card p-8 card-hover w-1/2 m-5 variant-filled-surface"
-		>
-			<p>
-				{data.homepageData[0].textWithImage.text}
-			</p>
-
-			<button type="button" class="btn bg-gradient-to-br variant-gradient-primary-secondary"
-				>See Our Class Schedule</button
-			>
-		</div>
-		{#if data.homepageData[0].textWithImage.image && data.homepageData[0].textWithImage.image.asset}
-			<img class="rounded" src={imageFor(data.homepageData[0].textWithImage.image)} alt="" />
-		{/if}
-	</div>
+<div class="lg:p-20">
+	{#if data.homepageData[0].textWithImage}
+		<TextWithImage textWithImage={data.homepageData[0].textWithImage} imageOnLeft />
+	{/if}
 </div>
