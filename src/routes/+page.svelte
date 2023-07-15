409e18d0b788cfa53b596a809bc07d361b6d73f9
Alexander Faus <alexfaus08@gmail.com>
2023-07-15 14:15:02 -0400

    feat: link to registration documents

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -86,6 +86,23 @@
 
 				{homepageData.email}
 			</a>
+			<a href="#documents" class="btn variant-filled-tertiary">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6 mr-1"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
+					/>
+				</svg>
+				Registration Documents
+			</a>
 		</div>
 	</Section>
 	<Section bgColor="bg-gradient-to-br variant-gradient-primary-secondary" id="classSchedule">
@@ -125,7 +142,7 @@
 			</AccordionItem>
 		</Accordion>
 	</Section>
-	<Section bgColor="bg-surface-600">
+	<Section bgColor="bg-surface-600" id="documents">
 		<h2 class="text-center text-4xl text-primary-400">{homepageData.registrationTagline}</h2>
 		<p class="p-4 text-lg md:text-2xl mb-4">
 			{homepageData.registrationDescription}
